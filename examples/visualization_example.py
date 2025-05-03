bb98a13ae01fd3610c77f77b3ce6e4d262129080
Alakarthika U <83805418+aki-au@users.noreply.github.com>
2025-05-03 22:36:17 +0000

    Delete examples directory

--- examples/visualization_example.py
+++
@@ -1,7 +0,0 @@
-from artutils.visualization import plot_color_swatch, plot_color_wheel_gradient
-from artutils.color_utils import generate_full_hsl_gradient
-
-palette = ['#123456', '#abcdef', '#fffffa', '#654321']
-plot_swatch(palette)
-gradient = generate_full_hsl_gradient(palette, steps_per_transition=100) 
-plot_wheel(gradient)
